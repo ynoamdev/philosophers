c7bf67c9209ffdcfe4239f78696e17f94fafa835
Youssef Noam <ynoam@e1r12p4.1337.ma>
2021-06-19 19:33:15 +0100

    youssef you can do it

--- philo/philo.c
+++ philo/philo.c
@@ -1,10 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int	ft_isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	return (0);
+}
+void	input_error(char *av[])
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	while (av[i])
+	{
+		j = 0;
+		while (av[i][j])
+			if (!ft_isdigit(av[i][j++]))
+				printf("error\n");
+		i++;
+	}
+}
+
 int	main(int ac, char *av[])
 {
-	if (ac == 6 && ac == 5)
+	if (ac == 6 || ac == 5)
 	{
+		input_error(av);
 	}
 	printf("error\n");
 	return (0);
